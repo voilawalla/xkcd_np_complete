041aeb0b9880b63773ae35fc3615ba914059d892
Lily-Therese Walla <voilawalla411@gmail.com>
2016-01-31 22:28:30 -0600

    Update currency.rb
    Clean up comments

--- lib/xkcd_tablexi/currency.rb
+++ lib/xkcd_tablexi/currency.rb
@@ -3,12 +3,12 @@
 	   #this allows RSpec to use these methods on Currency itself
 	   module_function
 
-		#strips dollar signs, decimal points and trailing spaces from	   
+	    #strips dollar signs, decimal points and trailing spaces from prices entered via CSV file	   
 	    def currency_to_i(currency)
 	      currency.gsub(/[^\d]/, '').to_i
 	    end
 
-		#reverses process
+	    #reverses process
 	    def integer_to_currency(integer)
 	      '$%.2f' % (integer * 0.01)
 	    end
